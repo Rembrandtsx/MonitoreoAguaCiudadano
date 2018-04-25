fda370fc106313419efd34487c8036241ca32ebe
Fabio <fa.espinosa10@uniandes.edu.co>
2018-04-25 12:08:46 -0500

    barra negra con dropdowns

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -9,9 +9,96 @@ class Sidebar extends Component {
 		const { color_grey_dark_1, color_primary, color_primary_light, color_grey_light_1 } = colors;
 		const { bp_medium, bp_small } = bp;
 		return (
-			<nav className="sidebar">
-				<ul className="side-nav">
-					<li className={`side-nav__item ${pathname === '/' && 'side-nav__item--active'}`}>
+			<nav className="navbar navbar-inverse" role="navigation">
+				<div className="container-fluid">
+					{/* <!-- Brand and toggle get grouped for better mobile display --> */}
+					<div className="navbar-header">
+						<button
+							type="button"
+							className="navbar-toggle"
+							data-toggle="collapse"
+							data-target="#bs-example-navbar-collapse-1">
+							<span className="sr-only">Toggle navigation</span>
+							<span className="icon-bar" />
+							<span className="icon-bar" />
+							<span className="icon-bar" />
+						</button>
+						{/* <a className="navbar-brand" href="#">
+									Brand
+								</a> */}
+					</div>
+
+					{/* <!-- Collect the nav links, forms, and other content for toggling --> */}
+					<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul className="nav navbar-nav">
+							<li className={`${pathname === '/' && 'active'}`}>
+								<Link to="/">INICIO</Link>
+							</li>
+							<li className={`dropdown ${pathname === '/comunidades' && 'active'}`}>
+								<a href="#" className="dropdown-toggle" data-toggle="dropdown">
+									COMUNIDADES <b className="caret" />
+								</a>
+								<ul className="dropdown-menu multi-level">
+									<li className="dropdown-submenu">
+										<a href="#">TEUSACA</a>
+										<ul className="dropdown-menu">
+											<li>
+												<a href="">Mapa</a>
+											</li>
+											<li>
+												<a href="">Actividades</a>
+											</li>
+											<li>
+												<a href="">Colaboradores</a>
+											</li>
+											<li>
+												<a href="">Patrocinadores</a>
+											</li>
+										</ul>
+									</li>
+									<li>
+										<a href="#">SANTURBAN</a>
+									</li>
+									<li>
+										<a href="#">LETICIA</a>
+									</li>
+								</ul>
+							</li>
+							<li className={`dropdown ${pathname === '/sonda' && 'active'}`}>
+								<a href="#" className="dropdown-toggle" data-toggle="dropdown">
+									SONDA DE AGUA <b className="caret" />
+								</a>
+								<ul className="dropdown-menu multi-level">
+									<li>
+										<a href="#">SENSOAGUA V1</a>
+									</li>
+									<li>
+										<a href="#">SENSOAGUA V2</a>
+									</li>
+									<li>
+										<a href="#">SENSOAGUA V3</a>
+									</li>
+								</ul>
+							</li>
+							<li className={`dropdown ${pathname === '/subir' && 'active'}`}>
+								<Link to="/subir">CARGAR DATOS</Link>
+							</li>
+							<li className={`dropdown ${pathname === '/app' && 'active'}`}>
+								<Link to="/app">APP</Link>
+							</li>
+							<li className={`dropdown ${pathname === '/multimedia' && 'active'}`}>
+								<Link to="/multimedia">MULTIMEDIA</Link>
+							</li>
+						</ul>
+						<ul className="nav navbar-nav navbar-right">
+							<li className={`dropdown ${pathname === '/contacto' && 'active'}`}>
+								<Link to="/contacto">CONTACTO</Link>
+							</li>
+						</ul>
+					</div>
+				</div>
+
+				{/* <li className={`side-nav__item ${pathname === '/' && 'side-nav__item--active'}`}>
 						<Link to="/" className="side-nav__link">
 							<span>Introducción</span>
 						</Link>
@@ -27,7 +114,7 @@ class Sidebar extends Component {
 						<Link to="/informacion" className="side-nav__link">
 							<span>Informacion</span>
 						</Link>
-					</li> */}
+					</li> *
 					<li className={`side-nav__item ${pathname === '/el-proyecto' && 'side-nav__item--active'}`}>
 						<Link to="/el-proyecto" className="side-nav__link">
 							<span>El Proyecto</span>
@@ -48,107 +135,65 @@ class Sidebar extends Component {
 							<span>Contacto</span>
 						</Link>
 					</li>
-				</ul>
+				</ul> */}
 				<style jsx global>{`
-					.sidebar {
-						background-color: ${color_grey_dark_1};
-						display: flex;
-						flex-direction: row;
-						justify-content: center;
+					/* Bootstrap nav */
+					.navbar-default .navbar-nav > li.dropdown:hover > a,
+					.navbar-default .navbar-nav > li.dropdown:hover > a:hover,
+					.navbar-default .navbar-nav > li.dropdown:hover > a:focus {
+						background-color: rgb(231, 231, 231);
+						color: rgb(85, 85, 85);
+					}
+					li.dropdown:hover > .dropdown-menu {
+						display: block;
 					}
 
-					.subir_datos {
-						@media ${bp_medium} {
-							display: none;
-						}
+					.dropdown-submenu {
+						position: relative;
 					}
 
-					.side-nav {
-						font-size: 1.4rem;
-						list-style: none;
-						margin-top: 1rem;
-						margin-bottom: 1rem;
-						display: flex;
-						justify-content: center;
+					.dropdown-submenu > .dropdown-menu {
+						top: 0;
+						left: 100%;
+						margin-top: -6px;
+						margin-left: -1px;
+						-webkit-border-radius: 0 6px 6px 6px;
+						-moz-border-radius: 0 6px 6px;
+						border-radius: 0 6px 6px 6px;
+					}
 
-						@media ${bp_medium} {
-							display: flex;
-							margin: 0;
-						}
+					.dropdown-submenu:hover > .dropdown-menu {
+						display: block;
+					}
 
-						&__item {
-							position: relative;
-							&:not(:last-child) {
-								margin-right: 1rem;
-								@media ${bp_medium} {
-									margin: 0;
-								}
-							}
+					.dropdown-submenu > a:after {
+						display: block;
+						content: ' ';
+						float: right;
+						width: 0;
+						height: 0;
+						border-color: transparent;
+						border-style: solid;
+						border-width: 5px 0 5px 5px;
+						border-left-color: #ccc;
+						margin-top: 5px;
+						margin-right: -10px;
+					}
 
-							@media ${bp_medium} {
-								font-size: 1.8em;
-								flex: 1;
-							}
-						}
+					.dropdown-submenu:hover > a:after {
+						border-left-color: #fff;
+					}
 
-						&__item::before {
-							content: '';
-							position: absolute;
-							top: 0;
-							left: 0;
-							height: 100%;
-							width: 3px;
-							background-color: ${color_primary};
-							transform: scaleY(0);
-							transform-origin: center;
-							transition: transform 0.2s, width 0.4s cubic-bezier(1, 0, 0, 1) 0.2s, background-color 0.2s;
-						}
+					.dropdown-submenu.pull-left {
+						float: none;
+					}
 
-						&__item:hover::before,
-						&__item--active::before {
-							transform: scaleY(1);
-							width: 100%;
-						}
-
-						&__item:active::before {
-							background-color: ${color_primary_light};
-						}
-
-						&__link:link,
-						&__link:visited {
-							color: ${color_grey_light_1};
-							text-decoration: none;
-							text-transform: uppercase;
-							display: block;
-							padding: 1.5rem 3rem;
-							display: flex;
-							align-items: center;
-							position: relative;
-							z-index: 100;
-
-							@media ${bp_medium} {
-								justify-content: center;
-								padding: 2rem;
-							}
-
-							@media ${bp_small} {
-								flex-direction: column;
-								padding: 1.5rem 0.5rem;
-							}
-						}
-
-						&__icon {
-							width: 1.75rem;
-							height: 1.75rem;
-							margin-right: 2rem;
-							fill: currentColor;
-							@media ${bp_small} {
-								margin-right: 0;
-								width: 1.5rem;
-								height: 1.5rem;
-								margin-bottom: 0.7rem;
-							}
-						}
+					.dropdown-submenu.pull-left > .dropdown-menu {
+						left: -100%;
+						margin-left: 10px;
+						-webkit-border-radius: 6px 0 6px 6px;
+						-moz-border-radius: 6px 0 6px 6px;
+						border-radius: 6px 0 6px 6px;
 					}
 				`}</style>
 			</nav>
