94862d7effba5415127f8d8883d6c8cc018a8d89
Camilo Andres Salinas Martinez <iam@camilosalinas.me>
2020-02-25 17:12:55 -0500

    Boyacá-Samacá Changes 2020

--- src/Screens/comunidades/Comunidades.js
+++ src/Screens/comunidades/Comunidades.js
@@ -18,8 +18,20 @@ class Comunidades extends Component {
 			<div className="detail">
 				<div className="description">
 					<h1>Comunidades</h1>
+					<div className="indice">
+						<h3>Índice</h3>
+						<ul>
+							<li><a href="#santurban">Santurban</a></li>
+							<li><a href="#teusaca">Teusacá</a></li>
+							<li><a href="#amazonas">Amazonas</a></li>
+							<li><a href="#baru">Barú</a></li>
+							<li><a href="#uniandes">Uniandes</a></li>
+							<li><a href="#eje-ambiental">Eje Ambiental</a></li>
+							<li><a href="#boyaca-samaca">Boyacá - Samacá</a></li>
+						</ul>
+					</div>
 					<div className="contenido">
-						<div>
+						<div id="santurban">
 							<Link to="/comunidades-santurban">
 								<div className="image-container">
 									<img src={santurban} />
@@ -28,44 +40,49 @@ class Comunidades extends Component {
 							</Link>
 						</div>
 
-						<div>
+						<div id="teusaca">
 							<Link to="/comunidades-teusaca">
 								<div className="image-container">
 									<img src={teusaca} />
 									<div className="after">Teusacá</div>
 								</div>
 							</Link>
-						</div><div>
+						</div>
+						<div id="amazonas">
 							<Link to="/comunidades-amazonas">
 								<div className="image-container">
 									<img src={amazonas} />
 									<div className="after">Amazonas</div>
 								</div></Link>
-						</div><div>
+						</div>
+						<div id="baru">
 							<Link to="/comunidades-baru">
 								<div className="image-container">
 									<img src={baru} />
 									<div className="after">Barú</div>
 								</div>
 							</Link>
-						</div><div>
+						</div>
+						<div id="uniandes">
 							<Link to="/comunidades-uniandes">
 							<div className="image-container">
 									<img src={uniandes} />
 									<div className="after">Uniandes</div>
 								</div>
 								</Link>
-						</div><div>
+						</div>
+						<div id="eje-ambiental">
 							<Link to="/comunidades-eje-ambiental">
 							<div className="image-container">
 									<img src={eje} />
 									<div className="after">Eje Ambiental</div>
 								</div></Link>
-						</div><div>
+						</div>
+						<div id="boyaca-samaca"> 
 							<Link to="/comunidades-boyaca">
 								<div className="image-container">
 									<img src={boyaca} />
-									<div className="after">Boyacá</div>
+									<div className="after">Boyacá - Samacá</div>
 								</div>
 							</Link>
 						</div>
@@ -79,7 +96,7 @@ class Comunidades extends Component {
 						padding: 4.5rem;
 						background-color: ${color_grey_light_1};
 						border-bottom: 1px solid ${color_grey_light_2};
-
+						
 						&:not(:last-child) {
 							/* margin-bottom: 10rem; */
 						}
@@ -92,7 +109,12 @@ class Comunidades extends Component {
 							flex-direction: column;
 						}
 					}
-
+					.indice{
+						margin-left: 5%;
+					}
+					.indice ul{
+						margin-left: 2em;
+					}
 					.contenido > div {
 						margin-top:5vh;
 					}
--- src/Screens/comunidades/boyaca/Boyaca.js
+++ src/Screens/comunidades/boyaca/Boyaca.js
@@ -6,16 +6,20 @@ import { bp, colors } from "../../../theme";
 
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import mapa from '../comunidadesGen/mapa.png';
-import img1 from './Boyaca- Samaca/img1.jpg';
-import img2 from './Boyaca- Samaca/img2.jpg'
-import img3 from './Boyaca- Samaca/img3.jpg'
-import img4 from './Boyaca- Samaca/img4.jpg'
-import img5 from './Boyaca- Samaca/img5.jpg'
-import img6 from './Boyaca- Samaca/img6.jpg';
+import mapa from "../comunidadesGen/mapa.png";
+import img1 from "./Boyaca- Samaca/img1.jpg";
+import img2 from "./Boyaca- Samaca/img2.jpg";
+import img3 from "./Boyaca- Samaca/img3.jpg";
+import img4 from "./Boyaca- Samaca/img4.jpg";
+import img5 from "./Boyaca- Samaca/img5.jpg";
+import img6 from "./Boyaca- Samaca/img6.jpg";
+import img7 from "./Boyaca- Samaca/img7.png";
+import img8 from "./Boyaca- Samaca/santotomas.jpeg";
+import img9 from "./Boyaca- Samaca/minciencias.png";
+import img10 from "./Boyaca- Samaca/boyacagob.png";
+import img11 from "./Boyaca- Samaca/decreto.jpeg";
 
-import uniandes from '../comunidadesGen/logo-uniandes.png'
-
+import uniandes from "../comunidadesGen/logo-uniandes.png";
 
 class Proyecto7 extends Component {
   render() {
@@ -42,22 +46,22 @@ class Proyecto7 extends Component {
     return (
       <div className="detail">
         <div className="description">
-        <h2>Boyacá</h2>
-          
-        <div className="image-container">
-									<img src={mapa} />
-									<div className="after">PROXIMAMENTE</div>
-				</div>
+          <h2>Boyacá - Samacá</h2>
+
+          <div className="image-container">
+            <img src={mapa} />
+            <div className="after">PROXIMAMENTE</div>
+          </div>
           <h3>Monitoreo de agua en comunidades vulnerables de Colombia</h3>
-      
-          <Carousel >
+
+          <Carousel>
             <div>
               <img src={img1} className="imagen" />
             </div>
             <div>
               <img src={img2} className="imagen" />
             </div>
-           
+
             <div>
               <img src={img3} className="imagen" />
             </div>
@@ -71,13 +75,150 @@ class Proyecto7 extends Component {
               <img src={img6} className="imagen" />
             </div>
           </Carousel>
+
+          <div>
+            <h3 className="centered">
+              Plataforma Comunitaria para el monitoreo en la calidad del agua en
+              la región de Samacá – Boyacá
+            </h3>
+            <p>
+              Según datos recogidos en el Esquema de Ordenamiento Territorial,
+              para el periodo 2015- 2027 del municipio (Samacá, 2016), la
+              actividad económica que más genera fuentes de empleo es la minería
+              de carbón y coquización; con una participación de alrededor del
+              35% de la población involucrada directamente en estas actividades.
+              Así mismo, asociado a estas actividades de extracción, transporte
+              y coquización de este mineral, se han reportado impactos y
+              deterioros ambientales en los enclaves mineros, que a su vez se
+              han traducido en un deterioro de la salud de los habitantes que se
+              dedican específicamente a esta actividad (Acosta Bueno, 2016).
+              Dentro de este contexto, se enmarca la necesidad desarrollar una
+              red ciudadana de monitoreo de bajo costo, portátil y fácil de
+              usar, con el cual se pueda definir un mapa de riesgo de la calidad
+              del agua que facilite a las comunidades, autoridades locales,
+              departamentales y nacionales la realización de un diagnóstico
+              basado en datos obtenidos en campo, sobre de los impactos
+              ambientales de la minería del carbón en las comunidades
+              circundantes acorde a los criterios recogidos (ver Tabla 1) en el
+              decreto 475 de 1998.
+            </p>
+            <img src={img11} className="tabla" />
+            <p className="centered">
+              <b>Decreto 1075 - 2015. </b>
+              <a href="https://www.lexbase.co/files/lexbase/D1075de2015.pdf">
+                https://www.lexbase.co/files/lexbase/D1075de2015.pdf{" "}
+              </a>
+            </p>
+            <p>
+              El resultado es que el municipio de Samacá (Boyacá) presenta unos
+              ecosistemas vulnerables los cuales están bajo amenaza crítica al
+              igual que la salud de las comunidades locales que dependen en sus
+              recursos de agua. La implementación de sistema detección y
+              monitoreo de contaminantes es primordial para la calidad del agua.
+              Tales sistemas pueden ayudar al diagnóstico de la fuente de
+              contaminación y que posteriormente pueden ser usados en
+              estrategias de prevención y gestión. Durante la propuesta
+              proyecto, se desarrollarán sistemas de detección asequibles para
+              detección en el sitio y en tiempo real de contaminantes en el agua
+              según las normas técnicas dispuestas en el “Decreto 475 de 1998
+              sobre la calidad de agua potable en Colombia”. (Barrera Niño,
+              2017). El actor en el proyecto es el ciudadano quien al estar
+              afectado se apropia de un problema constructivamente a través de
+              tecnologías de monitoreo que le permiten visualizarlo y generar el
+              soporte necesario para llegar a las autoridades.
+              <br />
+              Debido a la multidisciplinariedad del proyecto, se establece una
+              colaboración entre la <b>Universidad Santo Tomás</b>, Seccional
+              Tunja <b> (USTA)</b>, la <b>Universidad de Los Andes (UA) </b> y
+              la empresa <b> Germán Franco Carbón SAS (GFC) </b>. Los
+              investigadores de Universidad Santo Tomás, Seccional Tunja
+              aportarán su experiencia en tecnologías de detección y control en
+              la calidad del agua, regulación y tratamiento. Los investigadores
+              de Universidad de Los Andes contribuirán con experiencia en
+              electrónica, nanofabricación incluidas tecnologías lab-on-a-chip,
+              y facilitará la creación de estaciones de muestreo de campo que
+              serán utilizados en el proyecto y la empresa{" "}
+              <b>Germán Franco Carbón SAS (GFC)</b>, cuya actividad principal es
+              la de generar productos a base de carbón, contribuirá en la
+              verificación y evaluación del impacto de sus desechos, generando
+              recomendaciones conjuntas sobre el uso y gestiones de riesgo en
+              agua dentro de la temática de emergencias y desastres que
+              comprometan el medio ambiente y recurso humano.
+            </p>
+          </div>
+
+          <div>
+            <h2 className="centered">
+              Noticias
+            </h2>
+            <center>
+
+            <h4>Video</h4>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/XW1o6Fwjtpk?start=4340" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> <br></br>
+            <h4>Visibilidad del proyecto en las universidades</h4>
+            <a href="https://electrica.uniandes.edu.co/en/home-ingles/9-espanol/departamento/noticias/671-uniandes-presente-en-taller-de-socializacion-en-samaca-boyaca ">https://electrica.uniandes.edu.co/en/home-ingles/9-espanol/departamento/noticias/671-uniandes-presente-en-taller-de-socializacion-en-samaca-boyaca</a>
+            <br>
+            </br>    
+            <h4>Seminario</h4>
+            <a href="https://iee.uniandes.edu.co/es/home/9-espanol/departamento/noticias/674-uniandes-organizo-seminario-sobre-los-avances-de-proyecto-">https://iee.uniandes.edu.co/es/home/9-espanol/departamento/noticias/674-uniandes-organizo-seminario-sobre-los-avances-de-proyecto-</a>
+            </center>
+          
+          
+          
+          </div>
+
           <h3>Patrocinadores y Colaboradores</h3>
           <div className="patrocinadores">
-            
           <div className="img_container">
-              <img src={uniandes} alt="" className="patrocinador" />
+              <div className="patrocinador">
+                <img src={img10} alt="" />
+                <p className="centered">
+                  <a href="https://www.boyaca.gov.co/">
+                    https://www.boyaca.gov.co/
+                  </a>
+                </p>
+              </div>
             </div>
-         
+            <div className="img_container">
+              <div className="patrocinador">
+                <img src={img9} alt="" />
+                <p className="centered">
+                  <a href="https://minciencias.gov.co/">
+                  https://minciencias.gov.co/
+                  </a>
+                </p>
+              </div>
+            </div>
+            <div className="img_container">
+              <div className="patrocinador">
+                <img src={img8} alt="" />
+                <p className="centered">
+                  <a href="https://www.ustatunja.edu.co/">
+                  https://www.ustatunja.edu.co/
+                  </a>
+                </p>
+              </div>
+            </div>
+            <div className="img_container">
+              <div className="patrocinador">
+                <img src={uniandes} alt="" />
+                <p className="centered">
+                  <a href="https://uniandes.edu.co/">
+                    https://uniandes.edu.co/
+                  </a>
+                </p>
+              </div>
+            </div>
+            <div className="img_container">
+              <div className="patrocinador">
+                <img src={img7} alt="" />
+                <p className="centered">
+                  <a href="https://www.germanfranco.com.co/ ">
+                    https://www.germanfranco.com.co/{" "}
+                  </a>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
 
@@ -93,7 +234,9 @@ class Proyecto7 extends Component {
             &:not(:last-child) {
               /* margin-bottom: 10rem; */
             }
-
+            .centered {
+              text-align: center;
+            }
             @media ${bp_medium} {
               padding: 3rem;
             }
@@ -102,7 +245,11 @@ class Proyecto7 extends Component {
               flex-direction: column;
             }
           }
-
+          .tabla {
+            position: relative;
+            left: 50%;
+            transform: translateX(-50%);
+          }
           .title {
             font-weight: bold;
             font-style: italic;
@@ -132,24 +279,27 @@ class Proyecto7 extends Component {
           .patrocinadores {
             display: flex;
             justify-content: space-around;
-
+            flex-direction: row;
+            flex-wrap: wrap;
             @media ${bp_large} {
               flex-direction: column;
               align-items: center;
             }
           }
-
-          .patrocinador {
+          
+          .patrocinador img {
             width: 20rem;
             height: auto;
           }
 
           .img_container {
+            width: 25%;
+            margin: 0 2.5%;
             display: flex;
             flex-direction: column;
             justify-content: center;
           }
-          p{
+          p {
             text-align: justify;
           }
         `}</style>
